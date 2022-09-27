ccec626cd5db759444e6e31cf63d35914ee95d78
JEFFe <leevi@i.jeffe.co>
2022-09-28 01:29:54 +0300

    hakuominaisuutta opiskelijoille

---
+++ test/OppilaatView.swift
@@ -0,0 +1,20 @@
+//
+//  OppilaatView.swift
+//  test
+//
+//  Created by Leevi on 28.9.2022.
+//
+
+import SwiftUI
+
+struct OppilaatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OppilaatView_Previews: PreviewProvider {
+    static var previews: some View {
+        OppilaatView()
+    }
+}
